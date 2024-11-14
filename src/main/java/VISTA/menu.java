c194331f3bad0a0616531169818d2db505eb3ac8
Gonzalo Or <95729955+gonzaor@users.noreply.github.com>
2024-11-13 23:48:33 -0300

    Actualizacion de menu.java
    Se agrego el menu principal de la aplicacion. con algunas funcionalidades.

--- src/main/java/VISTA/menu.java
+++ src/main/java/VISTA/menu.java
@@ -1,4 +1,19 @@
 package VISTA;
 
 public class menu {
+  
+    public static void mostrarMenu(String dbStatus) {
+        System.out.println("╔══════════════════════════════════════╗");
+        System.out.println("║   Sistema de gestión de ASUR         ║");
+        System.out.println("╠══════════════════════════════════════╣");
+        System.out.println("║ 1. Gestión de Usuarios               ║");
+        System.out.println("║ 2. Gestión de Perfiles               ║");
+        System.out.println("║ 3. Gestión de Funcionalidades        ║");
+        System.out.println("║ 4. Gestión de Pagos                  ║");
+        System.out.println("╠══════════════════════════════════════╣");
+        System.out.println("║ Estado de la Base de Datos: " + dbStatus + "  ║");
+        System.out.println("╚══════════════════════════════════════╝");
+    }
+
+  
 }
