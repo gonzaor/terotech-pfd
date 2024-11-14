95afd48ad20f6d737efb173d3affa4b5fe2a5a0d
gonza <gonza@gonza.org>
2024-11-13 22:08:51 -0300

    asddsad

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,21 @@
+import CONTROLADOR.Conexion;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.SQLException;
+
+
+
 public class main {
 
-    //asds
+    public static void main(String[] args) {
+         try {
+            Conexion conexion =
+                    Conexion.obtenerInstancia();
+            System.out.println("Conexión a la base de datos establecida: " + (conexion.getConexion() != null));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
